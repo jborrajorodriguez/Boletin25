a98dc8788af68aa93f2b4613fb1610e82711d4f9
Clase <Clase@Clase-PC>
2018-04-16 09:32:07 +0200

    Inicio

---
+++ src/boletin/pkg25/Boletin25.java
@@ -0,0 +1,15 @@
+
+package boletin.pkg25;
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class Boletin25 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
